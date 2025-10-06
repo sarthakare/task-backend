acf2013cd7ceee02476ceacccf3ee2b232ff861c
Sarthak <thakresarthak6@gmail.com>
2025-10-06 15:38:00 +0530

    increased the session timeout to 12hrs

--- app/utils/security.py
+++ app/utils/security.py
@@ -20,6 +20,6 @@ def verify_password(plain_password, hashed_password):
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=360))
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=720))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
